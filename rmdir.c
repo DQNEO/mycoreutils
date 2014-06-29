d5279742d452424f22d13cd77804214d1fbc22d8
DQNEO <dqneoo@gmail.com>
2014-06-29 21:18:45 +0900

    implemented rmdir

---
+++ rmdir.c
@@ -0,0 +1,25 @@
+/**
+ * rmdir command
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+	fprintf(stderr, "%s: missing operand\n", argv[0]);
+	return 1;
+    }
+
+    int i;
+    int ret = 0;
+
+    for (i = 1; i <= argc - 1; i++) {
+	if (rmdir(argv[i]) != 0) {
+	    perror(argv[i]);
+	    ret = 1;
+	}
+    }
+
+    return ret;
+}
