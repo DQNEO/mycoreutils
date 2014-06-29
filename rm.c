acca5e60a253e1424f5b0ca31dd1b26f61cf908e
DQNEO <dqneoo@gmail.com>
2014-06-29 21:32:04 +0900

    implemented rm command

---
+++ rm.c
@@ -0,0 +1,24 @@
+/**
+ * rm command
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+	fprintf(stderr, "%s: missing operand\n", argv[0]);
+	return 1;
+    }
+
+    int ret = 0;
+    int i;
+    for (i = 1; i <= argc - 1; i++) {
+	if (unlink(argv[i]) != 0) {
+	    perror(argv[i]);
+	    ret = 1;
+	}
+    }
+
+    return ret;
+}
